d94ec9ff43476bf6523be43bbed138cc3f30dccc
Stefano Torresi <webdev@stefanotorresi.it>
2014-04-22 17:00:10 +0200

    add zf2 modulemanager compatibility

--- src/Module.php
+++ src/Module.php
@@ -7,7 +7,51 @@
 
 namespace Thorr\Persistence;
 
-class Module
-{
+use Zend\ModuleManager\Feature;
 
+class Module implements
+    Feature\AutoloaderProviderInterface,
+    Feature\ConfigProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getAutoloaderConfig()
+    {
+        return [
+            'Zend\Loader\ClassMapAutoloader' => [
+                __DIR__ . '/../autoload_classmap.php',
+            ],
+            'Zend\Loader\StandardAutoloader' => [
+                'namespaces' => [
+                    __NAMESPACE__ => __DIR__,
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfig()
+    {
+        return [
+            /**
+             * Doctrine mappings for Entity\BaseEntity
+             */
+            'doctrine' => [
+                'driver' => [
+                    __NAMESPACE__ => [
+                        'class' => 'Doctrine\ORM\Mapping\Driver\XmlDriver',
+                        'paths' => __DIR__ . '/../config',
+                    ],
+                    'orm_default' =>[
+                        'drivers' => [
+                            __NAMESPACE__ . '\Entity' => __NAMESPACE__
+                        ]
+                    ]
+                ]
+            ],
+        ];
+    }
 }
