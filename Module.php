21f3f460ac456f7dfb97445eb34c43b12fec2287
Stefano Torresi <webdev@stefanotorresi.it>
2014-10-16 12:51:38 +0200

    remove Module.php redundancy

--- Module.php
+++ Module.php
@@ -3,10 +3,54 @@
  * @author Stefano Torresi (http://stefanotorresi.it)
  * @license See the file LICENSE.txt for copying permission.
  * ************************************************
- *
- * This file is placed here for compatibility with Zendframework 2's ModuleManager.
- * It allows usage of this module even without composer.
- * The original Module.php is in 'src/{ModuleNamespace}' in order to respect PSR-0
  */
 
-require_once __DIR__ . '/src/Module.php';
+namespace Thorr\Persistence;
+
+use Zend\ModuleManager\Feature;
+use Zend\ModuleManager\Listener\ServiceListenerInterface;
+use Zend\ModuleManager\ModuleManagerInterface;
+use Zend\ServiceManager\ServiceManager;
+
+class Module implements
+    Feature\InitProviderInterface,
+    Feature\ServiceProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function init(ModuleManagerInterface $moduleManager)
+    {
+        /** @var ServiceManager $serviceManager */
+        $serviceManager = $moduleManager->getEvent()->getParam('ServiceManager');
+
+        /** @var ServiceListenerInterface $serviceListener */
+        $serviceListener = $serviceManager->get('ServiceListener');
+
+        $serviceListener->addServiceManager(
+            'Thorr\Persistence\DataMapper\Manager\DataMapperManager',
+            'data_mapper_manager',
+            'Thorr\Persistence\DataMapper\Manager\DataMapperManagerConfigProviderInterface',
+            'getDataMapperManagerConfig'
+        );
+    }
+
+    /**
+     * Expected to return \Zend\ServiceManager\Config object or array to
+     * seed such an object.
+     *
+     * @return array|\Zend\ServiceManager\Config
+     */
+    public function getServiceConfig()
+    {
+        return [
+            'factories' => [
+                'Thorr\Persistence\DataMapper\Manager\DataMapperManager' =>
+                    'Thorr\Persistence\DataMapper\Manager\DataMapperManagerFactory'
+            ],
+            'aliases' => [
+                'DataMapperManager' => 'Thorr\Persistence\DataMapper\Manager\DataMapperManager'
+            ]
+        ];
+    }
+}
