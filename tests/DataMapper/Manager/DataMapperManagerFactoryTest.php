f5c6112a43153749f77aa9cb02c7b0b6b45bb8ae
Stefano Torresi <stefano@torresi.io>
2016-10-29 14:29:26 +0200

    add missing test for config initialization

--- tests/DataMapper/Manager/DataMapperManagerFactoryTest.php
+++ tests/DataMapper/Manager/DataMapperManagerFactoryTest.php
@@ -8,6 +8,8 @@
 namespace Thorr\Persistence\Test\DataMapper\Manager;
 
 use PHPUnit_Framework_TestCase as TestCase;
+use Thorr\Persistence\Test\Asset;
+use Thorr\Persistence\DataMapper\DataMapperInterface;
 use Thorr\Persistence\DataMapper\Manager\DataMapperManager;
 use Thorr\Persistence\DataMapper\Manager\DataMapperManagerFactory;
 use Zend\ServiceManager\Di\DiAbstractServiceFactory;
@@ -50,5 +52,32 @@ class DataMapperManagerFactoryTest extends TestCase
         $dataMapperManager = $factory->createService($serviceLocator);
 
         $this->assertAttributeContains($diAbstractFactory, 'abstractFactories', $dataMapperManager);
+    }
+
+    public function testCreateServiceWithConfig()
+    {
+        $factory        = new DataMapperManagerFactory();
+        $serviceLocator = $this->getMock(ServiceLocatorInterface::class);
+        $dataMapper     = $this->getMock(DataMapperInterface::class);
+        $dataMapper->expects($this->any())->method('getEntityClass')->willReturn(Asset\Entity::class);
+
+        $serviceLocator
+            ->expects($this->any())
+            ->method('get')
+            ->with('Config')
+            ->willReturn([
+                'thorr_persistence_dmm' => [
+                    'services' => [
+                        'foo' => $dataMapper
+                    ]
+                ]
+            ])
+        ;
+
+        $dataMapperManager = $factory->createService($serviceLocator);
+
+        $this->assertInstanceOf(DataMapperManager::class, $dataMapperManager);
+
+        $this->assertSame($dataMapper, $dataMapperManager->get('foo'));
     }
 }
